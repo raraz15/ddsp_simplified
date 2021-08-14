c6cf0c7cfcb4d9392c2c660fd1e1514762e73c80
hbalim15 <hbalim15@login03.kuacc.ku.edu.tr>
2021-08-14 23:18:47 +0300

    argparse train_supervised
     Author:    Recep Oğuz Araz <raraz15@ku.edu.tr>
     Committer: raraz15 <raraz15@ku.edu.tr>

--- callbacks.py
+++ callbacks.py
@@ -5,11 +5,11 @@ WANDB_API_KEY = "52c84ab3f3b5c1f999c7f5f389f5e423f46fc04a"
 
 class CustomWandbCallback(Callback):
 
-    def __init__(self, run_name, **kwargs):
+    def __init__(self, run_name, project_name='Supervised_Violin', **kwargs):
         super().__init__(**kwargs)
 
         wandb.login()
-        wandb.init(project='Supervised_Violin', entity='ddsp', name=run_name)
+        wandb.init(project=project_name, entity='ddsp', name=run_name)
         
     def on_epoch_end(self, epoch, logs=None):
         wandb.log(logs)
@@ -22,3 +22,10 @@ class ModelCheckpoint(Callback):
 
     def on_epoch_end(self, epoch, logs=None):
         self.model.save("{}/{}/model.ckpt".format(self.run_name, epoch))
+
+#class BestModelChekpoint:
+#    def __init__(self, model, run_name, **kwargs):
+#
+#        self.model = model
+#        self.run_name = run_name
+
--- train_supervised.py
+++ train_supervised.py
@@ -3,33 +3,34 @@ import os, argparse
 import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.optimizers.schedules import ExponentialDecay
+from tensorflow.keras.callbacks import ModelCheckpoint
 
 from preprocessing import F0LoudnessPreprocessor
 from encoders import SupervisedEncoder
 from models import SupervisedAutoencoder
 from decoders import DecoderWithoutLatent, DecoderWithLatent
 from losses import SpectralLoss, MultiLoss
-from callbacks import CustomWandbCallback, ModelCheckpoint
-from dataloader import make_violin_set , make_supervised_dataset
+from callbacks import CustomWandbCallback #, ModelCheckpoint
+from dataloader import make_supervised_dataset
 
 SAMPLE_RATE = 16000
 
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='Supervised Training Parameters.')
-    #parser.add_argument('-d', '--dataset-path', type=str, help='Dataset Path')
+    parser.add_argument('-p', '--path', type=str, help='Dataset Path')
+    parser.add_argument('-n', '--name', type=str, help='Run Name')
     parser.add_argument('-b', '--batch-size', type=int, default=32)
     parser.add_argument('-t', '--preprocessing-time', type=int, help='Preprocessing timesteps', default=250)
     parser.add_argument('-d', '--decoder-time', type=int, help='Decoder timesteps', default=1000)
     parser.add_argument('-e', '--encoder', type=str, help='Use an encoder or not. (mfcc will be extracted)', default="False")
     parser.add_argument('-r', '--reverb', type=str, help='Add reverb', default="True")
     parser.add_argument('-l', '--loss', type=str, help='Loss Type', default='spectral')
-    parser.add_argument('-n', '--name', type=str, help='Run Name')
     parser.add_argument('-w', '--wandb', type=str, help='Use Wandb', default="False")
     args = parser.parse_args()
     
-    #train_set, validation_set, _ = make_supervised_dataset(args.dataset_path)
-    train_set, validation_set, _ = make_violin_set(args.batch_size, args.encoder=="True")
+    # Make the Dataset
+    train_set, validation_set, _ = make_supervised_dataset(args.path, args.encoder=="True", args.batch_size)
     print('Dataset created.')
     preprocessor = F0LoudnessPreprocessor(timesteps=args.preprocessing_time)
     if args.encoder == "True":
@@ -47,8 +48,9 @@ if __name__ == '__main__':
                                 tracker_names=tracker_names,
                                 add_reverb=args.reverb)
     adam = Adam(learning_rate=ExponentialDecay(1e-3, decay_steps=10000, decay_rate=0.98))
-    csv_logger = tf.keras.callbacks.CSVLogger("logs/{}.csv".format(args.name), separator=",", append=False)
-    callbacks = [ModelCheckpoint(model, args.name), csv_logger]
+    callbacks = [ModelCheckpoint(filepath="model_checkpoints/{}/model.ckpt".format(args.name),
+                                monitor='val_spec_loss' if loss.name=='spectral_loss' else 'val_total_loss',
+                                save_best_only=True)]
     if args.wandb=="True":
         callbacks.append(CustomWandbCallback(args.name))
     model.compile(adam)
