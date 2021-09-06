077326f845e668ae5cb4e763272a3b2b3056b17f
Sergey Grechin <grechin.sergey@gmail.com>
2021-09-06 10:53:51 +0300

    [ddsp-simplified-2-midi-features #2] some lightweight tests for the midi file loader

--- tests/utils/midi_loader_test.py
+++ tests/utils/midi_loader_test.py
@@ -12,4 +12,11 @@ class MyTestCase(unittest.TestCase):
         res = loader.load(test_midi_file_path, 250, 4)
 
         keys = res.keys()
-        pass
+
+        self.assertEquals(['cc_55', 'velocity', 'pitch'], list(keys))
+        self.assertEqual(1000, res['cc_55'].shape[0])
+        self.assertEqual(1000, res['velocity'].shape[0])
+        self.assertEqual(1000, res['pitch'].shape[0])
+
+        self.assertEquals(0, min(res['velocity']))
+        self.assertEquals(0, min(res['pitch']))
