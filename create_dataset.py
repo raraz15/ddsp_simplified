ef1f5a6c28fa0fc7db183b681d69715ee6025c86
oguz_araz <raraz15@ku.edu.tr>
2021-10-02 12:46:41 +0000

    about datasets

--- create_dataset.py
+++ create_dataset.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Supervised Dataset Parameters.')
     parser.add_argument('-a', '--audio-path', type=str, required=True, help='Path to the audio files.')
     parser.add_argument('-d', '--dataset-name', type=str, required=True, help='Name of the new dataset.')
-    parser.add_argument('-f', '--frame-len', type=int, default=4, help='Duration of a frame in seconds.')
+    parser.add_argument('-f', '--dur-frame', type=int, default=4, help='Duration of a frame in seconds.')
     parser.add_argument('-n', '--normalize', action="store_true", help='Normalize audio files.')
     parser.add_argument('-c', '--conf-threshold', type=float, default=0.0, help='Confidence level threshold for the F0 extraction.')
     parser.add_argument('-m', '--mfcc', action="store_true", help='Extract mfcc features.')
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     for file in glob.glob(args.audio_path+'/*.mp3') + glob.glob(args.audio_path+ '/*.wav'):
         print(os.path.basename(file))  
         track = load_track(file, sample_rate=SAMPLE_RATE, normalize=args.normalize)
-        frames.append(frame_generator(track, args.frame_len*SAMPLE_RATE))
+        frames.append(frame_generator(track, args.dur_frame*SAMPLE_RATE))
     frames = np.concatenate(frames, axis=0)   
     
     trainX, valX = train_test_split(frames)
